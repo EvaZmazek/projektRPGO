f5b225f447578170072bd6df0ca17ad5012b0454
evazmazek <eva.zmazek@gmail.com>
2019-01-13 11:06:26 +0100

    odstranitev sumnikov

--- implementacija/risi_kocko.m
+++ implementacija/risi_kocko.m
@@ -1,6 +1,6 @@
 function risi_kocko(K, barva)
-% sprejme koordinate oglišè K (dobljene iz funkcije "kocka") in barvo
-% ter jo nariše v figuro
+% sprejme koordinate oglisc K (dobljene iz funkcije "kocka") in barvo
+% ter jo narise v figuro
 
 ploskve = [1 2 3 4; 2 6 7 3; 4 3 7 8; 1 5 8 4; 1 2 6 5; 5 6 7 8]; 
 patch('Vertices', K, 'Faces', ploskve, 'FaceColor', barva);
