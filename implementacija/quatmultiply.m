80145f77710871414351acbe03e79e533e19a2da
Eva Zmazek <eva.zmazek@gmail.com>
2019-01-13 11:34:17 +0100

    dodane potrebne funkcije za delovanje main

--- implementacija/quatmultiply.m
+++ implementacija/quatmultiply.m
@@ -9,17 +9,17 @@ function c = quatmultiply(a,b)
 % c ... produkt (nov kvaternion)
 
 c = zeros(1,4);
-a_s = a[1];
-b_s = b[1];
+a_s = a(1);
+b_s = b(1);
 
 a_v = quat_vec(a);
 b_v = quat_vec(b);
 
-c[1] = a_s * b_s - a_v * b_v;
+c(1) = a_s * b_s - a_v * b_v';
 c_v = a_s * b_v + b_s * a_v + cross(a_v,b_v);
 
-c[2] = c_v[1];
-c[3] = c_v[2];
-c[4] = c_v[3];
+c(2) = c_v(1);
+c(3) = c_v(2);
+c(4) = c_v(3);
 end
 
